b54c78bd87754daf549678676843c0e4922a03e4
conwid <akos@dotnetfalcon.com>
2019-01-13 21:07:22 +0100

    Add files via upload

---
+++ CodeGeneratorTemplate/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $rootnamespace$
+{
+    // Don't forget to add an instance of this class to your DbConfiguration descendant's constructor
+    // this.CodeGenerator = new $safeitemrootname$();
+    
+    // Override the methods to add custom generation steps to a specific model element
+    // see https://github.com/aspnet/EntityFramework6/blob/master/src/EntityFramework/Migrations/Design/CSharpMigrationCodeGenerator.cs
+	internal class $safeitemrootname$ : CSharpMigrationCodeGenerator
+	{
+	}
+}
---
+++ SqlGeneratorTemplate/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using System.Data.Entity.SqlServer;
+
+namespace $rootnamespace$
+{
+    // Don't forget to add an instance of this class to your DbConfiguration descendant's constructor
+    // SetSqlGenerator("System.Data.SqlClient", new $safeitemrootname$(this));                    
+    
+    // Override the methods to add custom generation steps to a specific model element
+    // see https://github.com/aspnet/EntityFramework6/blob/master/src/EntityFramework.SqlServer/SqlServerMigrationSqlGenerator.cs 
+    internal class $safeitemrootname$ : SqlServerMigrationGenerator
+	{
+	}
+}
